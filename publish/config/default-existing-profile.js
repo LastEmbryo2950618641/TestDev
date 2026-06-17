530e47fc733924462ad99def14b97a42a00cb732
game-3033362 <game@studio.local>
2026-06-17 06:58:17 +0000

    同步默认资料内联 JS

--- publish/config/default-existing-profile.js
+++ publish/config/default-existing-profile.js
@@ -1,5 +1,5 @@
 window.GameModules = window.GameModules || {};
 window.GameModules.inlineMd = window.GameModules.inlineMd || {};
-window.GameModules.inlineMd.defaultExistingProfile = "# 默认账号资料\n\n姓名：刘悠\n性别：男\n生日：1998-11-19\n具体地址：四川省成都市武侯区玉林街道玉林北路社区锦苑小区3栋2单元601号\n现实身份：程序工程师，计算机科学与技术硕士\n居住状态：与15岁双胞胎妹妹刘思瑶、刘思琪同住\n父母信息：父母已故\n父母去世原因：交通事故去世，具体细节不公开\n人际关系：双胞胎妹妹之一：刘思瑶；双胞胎妹妹之二：刘思琪\n备注：刘思瑶、刘思琪是15岁双胞胎妹妹，除姓名和独立人格外，日常生活处境相近。两人与刘悠关系亲近，依赖哥哥的照顾，也有各自独立的学习、生活节奏和个人边界。刘悠作为家庭支柱承担照顾责任，重视家人的安全、学业与日常生活。\n";
+window.GameModules.inlineMd.defaultExistingProfile = "# 默认账号资料\n\n姓名：刘悠\n性别：男\n生日：1998-11-19\n具体地址：四川省成都市武侯区玉林街道玉林北路社区锦苑小区3栋2单元601号\n现实身份：程序工程师，计算机科学与技术硕士\n居住状态：与15岁双胞胎妹妹刘思瑶、刘思琪同住\n父母信息：父母已故\n父母去世原因：交通事故去世，具体细节不公开\n人际关系：双胞胎妹妹之一：刘思瑶；双胞胎妹妹之二：刘思琪\n备注：刘思瑶、刘思琪是15岁萝莉双胞胎妹妹，除姓名和独立人格外，外貌、穿着偏好与同居处境相近，且对刘悠哥哥爱情与肉欲与占有欲均为100。两人依赖刘悠，有兄控和占有倾向，在家常以精致打扮、黏人互动试探刘悠态度，但担心他过于正经而保持克制。刘悠未明确察觉两人的复杂心思，只认为妹妹格外讲究。两人黑长直、黑瞳，常穿JK风制服与黑色过膝袜或者黑色裤袜，身高155cm，体态娇小。刘悠对妹妹有保护欲和占有倾向，但顾虑现实伦理与妹妹感受，始终保持距离。\n";
 window.GameModules.inlineMdSources = window.GameModules.inlineMdSources || {};
 window.GameModules.inlineMdSources.defaultExistingProfile = "default-existing-profile.md";
