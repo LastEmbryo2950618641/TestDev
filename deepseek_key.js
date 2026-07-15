0ea3bd1e9b7b975a046dc9505cd7cecb6d07e8ab
liuqi <2950618641@qq.com>
2026-07-15 22:14:19 +0800

    add bundled api key files

---
+++ deepseek_key.js
@@ -0,0 +1,3 @@
+window.GameModules = window.GameModules || {};
+window.GameModules.generatedKeys = window.GameModules.generatedKeys || {};
+window.GameModules.generatedKeys.deepseekKey = "sk-e60a8e67317f4f3698930ba2af20453e";
---
+++ pixatart_key.js
@@ -0,0 +1,3 @@
+window.GameModules = window.GameModules || {};
+window.GameModules.generatedKeys = window.GameModules.generatedKeys || {};
+window.GameModules.generatedKeys.pixaiKey = "sk-8OEzomCMud56YUhekjE0kElLirDivQtYT7ivDQblrtvoGi9d";
